27171b48b94b19080a04058eac3a597c6b2f1ccf
codankra <codankra@gmail.com>
2019-04-16 12:03:05 -0400

    general logic for c++

--- mod11.cpp
+++ mod11.cpp
@@ -22,46 +22,26 @@ int main(int argc, char **argv) {
 	int num[strlen(input)];
 	for (int i = 0; i < strlen(input); ++i){
 		if (input[i] - '0' >=0 && input[i] - '0'<10)
-		{
 			num[i] = input[i] - '0';
-		}
 		else{
 			printf("Unexpected argument: unable to properly convert input to number!\n");
 			return -1;
 		}
 	}
-	int size = sizeof(num)/sizeof(num[0]);
-	int rem;
-
-	//Size cases
-	if (size == 1) {
-		rem = num[0];
-	} else if (size == 2){
-		rem = num[1] - num[0];
-	} else if (size == 3) {
-		rem = num[0] +  num[2] - num[1];
-	/*
-	} else if (size == 4){ // *ONLY CONSISTENTLY WORKS 1100-1199
-		rem = num[0] +  num[3] - num[1] - num[2];
-	*/
-	}else{
-		fprintf(stderr, 
-			"WARNING: invalid size of number: results might be inaccurate!!\n");
-	}
-	if (neg) {
+	int size = strlen(input);
+	int rem = 0;
+	for (int i = 0; i < size; ++i)
+		rem = num[i] - rem;
+	if (neg) //only if # started with -
 		rem = -rem;
-	}
-	while(rem<0){
+	while(rem<0) //eg. -1 -=- 10
 		rem+=11;
-	}
-	while(rem>10){
+	while(rem>10) //eg. 12 -=- 1
 		rem-=11;
-	}
-	if (!rem){
+	if (!rem)
 		printf("%s is divisible by 11!\n", argv[1]);
-	} else{
+	else 
 		printf("%s is not divisible by 11. It is congruent (mod 11) to %d! (remainder /11 is %d)\n", argv[1], rem, neg ? 11-rem : rem);
-	}
 	return 1;
 }
 
